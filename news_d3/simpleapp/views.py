367a7544f591b95c86cd90bedfac189ee203ccee
leroyspb <leroy777@bk.ru>
2024-04-12 07:11:41 +0300

    001 test branch

--- news_d3/simpleapp/views.py
+++ news_d3/simpleapp/views.py
@@ -1,9 +1,6 @@
-from django.shortcuts import render
-
-# Create your views here.
 # Импортируем класс, который говорит нам о том,
 # что в этом представлении мы будем выводить список объектов из БД
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Product
 
 
@@ -19,3 +16,11 @@ class ProductsList(ListView):
     # Его надо указать, чтобы обратиться к списку объектов в html-шаблоне.
     context_object_name = 'products'
 
+
+class ProductDetail(DetailView):
+    # Модель всё та же, но мы хотим получать информацию по отдельному товару
+    model = Product
+    # Используем другой шаблон — product.html
+    template_name = 'product.html'
+    # Название объекта, в котором будет выбранный пользователем продукт
+    context_object_name = 'product'
