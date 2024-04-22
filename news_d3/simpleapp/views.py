f7b343f076fdfde35515b0cb64d9c25b15484484
leroyspb <leroy777@bk.ru>
2024-04-22 06:47:35 +0300

    22_04_24

--- news_d3/simpleapp/views.py
+++ news_d3/simpleapp/views.py
@@ -1,6 +1,8 @@
 # Импортируем класс, который говорит нам о том,
 # что в этом представлении мы будем выводить список объектов из БД
 from django.views.generic import ListView, DetailView
+
+from .filters import ProductFilter
 from .models import Product
 from datetime import datetime
 from django.http import HttpResponse
@@ -20,6 +22,19 @@ class ProductsList(ListView):
     ordering = '-date_create_in'
     paginate_by = 2
 
+    # Переопределяем функцию получения списка товаров
+    def get_queryset(self):
+        # Получаем обычный запрос
+        queryset = super().get_queryset()
+        # Используем наш класс фильтрации.
+        # self.request.GET содержит объект QueryDict, который мы рассматривали
+        # в этом юните ранее.
+        # Сохраняем нашу фильтрацию в объекте класса,
+        # чтобы потом добавить в контекст и использовать в шаблоне.
+        self.filterset = ProductFilter(self.request.GET, queryset)
+        # Возвращаем из функции отфильтрованный список товаров
+        return self.filterset.qs
+
 
 
 # Метод get_context_data позволяет нам изменить набор данных,
@@ -35,7 +50,11 @@ class ProductsList(ListView):
         # Добавим ещё одну пустую переменную,
         # чтобы на её примере рассмотреть работу ещё одного фильтра.
         context['next_sale'] = "Распродажа в среду!"
+        context['filterset'] = self.filterset
         return context
+
+
+
 
 class ProductDetail(DetailView):
     # Модель всё та же, но мы хотим получать информацию по отдельному товару
