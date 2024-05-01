b47ff53cac89fdf6aee49d05dccb7e6c7a3f202c
leroyspb <leroy777@bk.ru>
2024-05-01 15:04:36 +0300

    start_page_add

--- news_d3/simpleapp/urls.py
+++ news_d3/simpleapp/urls.py
@@ -12,10 +12,10 @@ urlpatterns = [
    # pk — это первичный ключ товара, который будет выводиться у нас в шаблон
    # int — указывает на то, что принимаются только целочисленные значения
    path('products/', ProductsList.as_view(), name='product_list'),
-   path('<int:pk>', ProductDetail.as_view(), name='product_detail'),
-   path('create/', ProductCreate.as_view(), name='product_create'),
-   path('<int:pk>/edit/', ProductUpdate.as_view(), name='product_edit'),
-   path('<int:pk>/delete/', ProductDelete.as_view(), name='product_delete'),
-   path('search/', SearchProduct.as_view(), name='search'),
+   path('products/<int:pk>', ProductDetail.as_view(), name='product_detail'),
+   path('products/create/', ProductCreate.as_view(), name='product_create'),
+   path('products/<int:pk>/edit/', ProductUpdate.as_view(), name='product_edit'),
+   path('products/<int:pk>/delete/', ProductDelete.as_view(), name='product_delete'),
+   path('products/search/', SearchProduct.as_view(), name='search'),
 
    ]
